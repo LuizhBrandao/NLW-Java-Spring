7e86093b41871066133191771d02dad5ef488e58
LuizhBrandao <luizh.brandaoo@gmail.com>
2025-02-23 13:37:01 -0300

    Subscripton controller Service and Exceptions

--- events/src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ events/src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,5 +1,9 @@
 package br.com.nlw.events.service;
 
+import br.com.nlw.events.dto.SubscriptionResponse;
+import br.com.nlw.events.exceptions.SubscriptionConflictException;
+import br.com.nlw.events.exceptions.EventNotFoundException;
+import br.com.nlw.events.exceptions.UserIndicadorNotFoundException;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
@@ -7,7 +11,9 @@ import br.com.nlw.events.repository.EventRepository;
 import br.com.nlw.events.repository.SubscriptionRepository;
 import br.com.nlw.events.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SubscriptionService {
 
     @Autowired
@@ -19,17 +25,34 @@ public class SubscriptionService {
     @Autowired
     private SubscriptionRepository subRepository;
 
-    public Subscription createNewSubscription(String eventName, User user){
+    public SubscriptionResponse createNewSubscription(String eventName, User user, Integer userId){
 
-        //Recuperar o evento pelo nome
+        //RECUPERAR O EVENTO PELO NOME
         Event evt = evtRepository.findByPrettyName(eventName);
-        user = userRepository.save(user);
+        if (evt == null){ //CASO ALTERNATIVO 2
+            throw new EventNotFoundException("Evento "+eventName+" não existe");
+        }
+        User userRec = userRepository.findByEmail(user.getEmail());
+        if (userRec == null) { // CASO ALTERNATIVO 1
+            userRec = userRepository.save(user);
+        }
+
+        User indicador = userRepository.findById(userId).orElse(null);
+        if (indicador == null){
+            throw new UserIndicadorNotFoundException("Usuario "+ userId +" indicador não existe");
+        }
 
         Subscription subs = new Subscription();
         subs.setEvent(evt);
-        subs.setSubscriber(user);
+        subs.setSubscriber(userRec);
+        subs.setIndication(indicador);
+
+        Subscription tmpSub = subRepository.findByEventAndSubscriber(evt, userRec);
+        if (tmpSub != null){ // CASO ALTERNATIVO 3
+            throw new SubscriptionConflictException("Ja existe inscrição para o usuário " + userRec.getName() + " no evento " + evt.getTitle());
+        }
 
         Subscription res = subRepository.save(subs);
-        return res;
+        return new SubscriptionResponse(res.getSubscriptionNumber(), "http//codecraft.com/subscription/"+ res.getEvent().getPrettyName()+"/"+res.getSubscriber().getId());
     }
 }
